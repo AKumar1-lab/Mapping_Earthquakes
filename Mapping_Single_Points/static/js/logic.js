bde1b34f1ed9ab516413a916653a40c5484679ed
Angela <angela_kumar@yahoo.com>
2021-09-22 13:21:34 -0700

    modified logic js file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,14 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+// Add Marker to the map
+// var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add location for Los Angeles Longitude and Latitude
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -13,3 +20,4 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
